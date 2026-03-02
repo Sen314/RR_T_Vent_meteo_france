bd6bb7917d92ff5830ff4dc946502706e9a85767
Sen314 <adrien.rachedi@gmail.com>
2026-03-02 13:51:49 +0100

    review test extreme temp variation

--- dbt/meteo_pipeline/tests/extreme_temp_variation.sql
+++ dbt/meteo_pipeline/tests/extreme_temp_variation.sql
@@ -4,4 +4,4 @@ SELECT
     fw.temp_min_c,
     fw.temp_max_c
 FROM {{ ref('int_weather_db_fact_weather') }} as fw
-WHERE fw.temp_min_c < -50 AND fw.temp_max_c > 60
+WHERE fw.temp_min_c < -50 OR fw.temp_max_c > 60
